a22b42c371ee51ed4238d21dfae7d6fe7e487d66
dasare <96350966+dasaren@users.noreply.github.com>
2023-05-08 06:08:18 +0000

    after revert

--- mysite/settings.py
+++ mysite/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'myusers.apps.MyusersConfig',
-      'chat.apps.ChatConfig',
-]
+#     'myusers.apps.MyusersConfig',
+#       'chat.apps.ChatConfig',
+# ]
 
 
 # AUTH_USER_MODEL = 'myusers.NewUser'
